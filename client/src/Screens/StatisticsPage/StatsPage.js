ddc38c183bd902c7081c930146526e5ec28ef308
prat1k-gupta <pratikgupta9773@gmail.com>
2022-08-23 04:05:05 +0530

    added cards to stats

--- client/src/Screens/StatisticsPage/StatsPage.js
+++ client/src/Screens/StatisticsPage/StatsPage.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import "./StatsStyles.css"
+import { Container } from 'react-bootstrap';
 import { MainScreen } from '../MainScreen'
-
+import { StatsCard } from './StatsCard';
+import stats from '../../data/stats';
 export const StatsPage = () => {
   return (
-    <MainScreen title="Connected People">These are your stats</MainScreen>
-  )
+    <MainScreen title="Connected People">
+      <Container>
+        {
+          stats.map((stat)=>{
+            return (
+              <StatsCard key={stat.id} stat={stat} /> 
+            );
+          })
+        }
+      </Container>
+    </MainScreen>
+  );
 }
