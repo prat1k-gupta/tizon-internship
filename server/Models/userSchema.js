f6adb8da4e961a9e5442d05ee03114fbe24e4ead
prat1k-gupta <pratikgupta9773@gmail.com>
2022-08-25 01:35:36 +0530

    added backend authentication

---
+++ server/Models/userSchema.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+require('dotenv').config(); 
+const userSchema  = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String, 
+            required: true, 
+            unique: true
+        },
+        password: { 
+            type: String, 
+            required: true
+        }, 
+        pic:{
+            type: String,
+            default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+        },
+        token: {
+            type: String
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+userSchema.pre('save',async function(){
+    if(this.isModified('password')){
+        this.password = await bcrypt.hashSync(this.password,12); 
+    }
+})
+
+userSchema.methods.generateAuthToken = async function(){
+    const SECRET_KEY = process.env.SECRET_KEY;
+    try{
+        let token = jwt.sign({_id: this._id}, SECRET_KEY); 
+        this.token =  token;
+        this.save(); 
+        return token; 
+    }catch(err){
+        throw err; 
+    }
+} 
+
+const user = mongoose.model("user",userSchema)
+
+module.exports = user; 
