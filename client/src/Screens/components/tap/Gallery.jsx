458bb7b22003f7fc1777265f5689a105f0d85790
SujeetYT <sujeet7982422191@gmail.com>
2022-08-30 15:34:15 +0530

    Gallery Completed

--- client/src/Screens/components/tap/Gallery.jsx
+++ client/src/Screens/components/tap/Gallery.jsx
@@ -17,11 +17,12 @@ const Gallery = ()=>{
         <>
             <div className={model ? "model open" : "model"}>
                 <img src={tempImg} alt="Image" />
+                <p className="closeIcon" onClick={()=>setModel(false)}>+</p>
             </div>
             <div className="gallery">
                 {data.map((item, index)=>{
                     return (
-                        <div className="pics" key={index} onClick={()=>{getImg(item.link)}}>
+                        <div className="pics" key={index} onClick={()=>getImg(item.link)}>
                             <img src={item.link} alt="Image" style={{width : "100%"}}/>
                         </div>
                     );
