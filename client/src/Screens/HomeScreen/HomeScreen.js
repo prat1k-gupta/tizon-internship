1c47d2a978ce0fa4d3bf29ef3856fc9db5231472
prat1k-gupta <pratikgupta9773@gmail.com>
2022-09-02 19:41:37 +0530

    fixed the folder structure and added homescreen

--- client/src/Screens/HomeScreen/HomeScreen.js
+++ client/src/Screens/HomeScreen/HomeScreen.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { useAuth } from '../../AuthContext/AuthContext';
 import { CardTap } from '../CardTap/CardTap';
 import LandingPage from '../LandingPage/LandingPage';
+import { WelcomeScreen } from '../WelcomeScreen/WelcomeScreen';
 
 export const HomeScreen = () => {
-  const {auth} = useAuth(); 
+  const {auth,business} = useAuth(); 
   return (
     <>
         {
-            auth ? <CardTap/> : <LandingPage/>
+            auth ? (business? <CardTap/>: <WelcomeScreen/> ) : <LandingPage/>
         }
     </>
   )
