ad692ea7af69977705329f7447baf8b23fe22402
prat1k-gupta <pratikgupta9773@gmail.com>
2022-09-02 22:21:04 +0530

    final colors for scroll area

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,84 +1,73 @@
-import axios from 'axios';
-import React from 'react'
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap"
-import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../AuthContext/AuthContext';
+import axios from "axios";
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../AuthContext/AuthContext";
 import logo from "./Tizonlogo.png";
 
 export const Header = () => {
-  const {user,setAuth,auth,business} = useAuth();  
-  console.log("business: ",business)
-  const navigate = useNavigate(); 
-  const handleLogout = async ()=>{
-    try{
-      const res = await axios.get('/api/logout');
-      if(res){
-        setAuth(false); 
-        navigate("/")
+  const { user, setAuth, auth, business } = useAuth();
+  console.log("business: ", business);
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      const res = await axios.get("/api/logout");
+      if (res) {
+        setAuth(false);
+        navigate("/");
       }
-    }catch(err){
-      console.log(err); 
+    } catch (err) {
+      console.log(err);
     }
-    
-  }
-  console.log("header: "+auth)
+  };
+  console.log("header: " + auth);
   return (
     <Navbar collapseOnSelect sticky="top" expand="sm" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          <div style={{width:"35px"}}><img src={logo} style={{width: "100%"}} alt = "logo"/></div>
+          <div style={{ width: "35px" }}><img src={logo} style={{ width: "100%" }} alt = "logo" /></div>
         </Navbar.Brand>
         <Nav style={{ display: "flex", flexDirection: "row" }}>
           {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav"> */}
-              {!auth && (
-                <>
-                  <Nav.Link className="me-3" as={Link} exact to="/login">
-                    Login
-                  </Nav.Link>
-                  <Nav.Link className="me-3" as={Link} exact to="/register">
-                    Register
-                  </Nav.Link>
-                </>
+          {!auth && (
+            <>
+              <Nav.Link className="me-3" as={Link} exact to="/login">
+                Login
+              </Nav.Link>
+              <Nav.Link className="me-3" as={Link} exact to="/register">
+                Register
+              </Nav.Link>
+            </>
+          )}
+          {auth && (
+            <>
+              {business ? (
+                <Nav.Link className="me-3" as={Link} exact to="/editbusiness">
+                  Edit Business
+                </Nav.Link>
+              ) : (
+                <Nav.Link className="me-3" as={Link} exact to="/addbusiness">
+                  Add Business
+                </Nav.Link>
               )}
-              {auth && (
-                <>
-                  {business ? (
-                    <Nav.Link
-                      className="me-3"
-                      as={Link}
-                      exact
-                      to="/editbusiness"
-                    >
-                      Edit Business
-                    </Nav.Link>
-                  ) : (
-                    <Nav.Link
-                      className="me-3"
-                      as={Link}
-                      exact
-                      to="/addbusiness"
-                    >
-                      Add Business
-                    </Nav.Link>
-                  )}
-                  <Nav.Link className="me-3" as={Link} exact to="/stats">
-                    Stats
-                  </Nav.Link>
-                  <NavDropdown title={user && user.name}>
-                    <NavDropdown.Item as={Link} exact to="/edit">
-                      Edit Profile
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item onClick={handleLogout}>
-                      Logout
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </>
-              )}
-            </Nav>
-            {/* </Navbar.Collapse> */}
-          </Container>
-        </Navbar>
+              <Nav.Link className="me-3" as={Link} exact to="/stats">
+                Stats
+              </Nav.Link>
+              <NavDropdown title={user && user.name}>
+                <NavDropdown.Item as={Link} exact to="/edit">
+                  Edit Profile
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item onClick={handleLogout}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            </>
+          )}
+        </Nav>
+        {/* </Navbar.Collapse> */}
+      </Container>
+    </Navbar>
   );
-}
+};
