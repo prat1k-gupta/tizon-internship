27329a8626399e60f8862e2af8ee6fdb74b8f90d
prat1k-gupta <pratikgupta9773@gmail.com>
2022-09-05 21:42:43 +0530

    fixed bottom navbar socials

--- client/src/Screens/AfterCardTap/tap/BottomNav/BottomNav.jsx
+++ client/src/Screens/AfterCardTap/tap/BottomNav/BottomNav.jsx
@@ -37,34 +37,49 @@ const BottomNav = ({business})=>{
 
     const [show, setShow] = useState(false);
 
-    return (<>
-        {show &&
-        <div className="popup-overlay">
+    return (
+      <>
+        {show && (
+          <div className="popup-overlay">
             <div className="pop">
-                <h1>#Socials</h1>
-                <hr />
-                <Facebook />
-                <Twitter />
-                <Linkedin/>
-                <Instagram/>
-                <Web/>
-                {/* <hr style={{margin : "0"}}/> */}
-                {/* Agar koi dikkat hui to iski vajah se ho sakti hai */}
-                <ActiveButton type="button" value="Dismiss" onClick={()=>setShow(false)}/>
+              <h1>#Socials</h1>
+              <hr />
+              <Facebook business={business} />
+              <Twitter business={business} />
+              <Linkedin business={business} />
+              <Instagram business={business} />
+              <Web business={business} />
+              {/* <hr style={{margin : "0"}}/> */}
+              {/* Agar koi dikkat hui to iski vajah se ho sakti hai */}
+              <ActiveButton
+                type="button"
+                value="Dismiss"
+                onClick={() => setShow(false)}
+              />
             </div>
-        </div>
-    
-    }
+          </div>
+        )}
         <div className="bottomNav">
-            <div className="navBox">
-                <div className="option" onClick={()=>Email()}><HiOutlineMail /></div>
-                <div className="option" onClick={()=>web()}><HiOutlineGlobeAlt /></div>
-                <div className="option" onClick={()=>contact()}><RiContactsBookFill style={{fill : "#b615dc", width : "45px"}}/></div>
-                <div className="option" onClick={()=>SendMessage()}><SiWhatsapp  /></div>
-                <div className="option" onClick={()=>setShow(true)}><HiHashtag /></div>
+          <div className="navBox">
+            <div className="option" onClick={() => Email()}>
+              <HiOutlineMail />
             </div>
-        </div>    
-    </>);
+            <div className="option" onClick={() => web()}>
+              <HiOutlineGlobeAlt />
+            </div>
+            <div className="option" onClick={() => contact()}>
+              <RiContactsBookFill style={{ fill: "#b615dc", width: "45px" }} />
+            </div>
+            <div className="option" onClick={() => SendMessage()}>
+              <SiWhatsapp />
+            </div>
+            <div className="option" onClick={() => setShow(true)}>
+              <HiHashtag business = {business}/>
+            </div>
+          </div>
+        </div>
+      </>
+    );
 }
 
 export { BottomNav };
