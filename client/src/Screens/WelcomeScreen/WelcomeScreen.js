5d23799d12ce9c58f6beb04bc81e2dabf24b597d
prat1k-gupta <pratikgupta9773@gmail.com>
2022-09-05 14:29:05 +0530

    added welcomescreen dynamic name

--- client/src/Screens/WelcomeScreen/WelcomeScreen.js
+++ client/src/Screens/WelcomeScreen/WelcomeScreen.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import { useAuth } from '../../AuthContext/AuthContext';
 import heroImg from "../../components/HeroImg.svg";
 import { ActiveButton } from '../components/main/Inputs';
 import "./welcomescreen.css";
 
 export const WelcomeScreen = () => {
-  const name = "Sujeet";
+  const {user} = useAuth();
   return (
   <div className='welcomeScreen'>
     <div className='heroSection'>
       <div className='heroText'>
-        <h1>Hello {name}</h1>
-        <p>Digitize your business card</p> 
+        <h1>Hello {user.name}</h1>
+        <p>Let's Digitize your business card</p> 
         <p>with</p> 
         <h3>Tizon</h3> 
       </div>
