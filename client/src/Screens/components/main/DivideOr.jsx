87173fcd42d1ce8cb31f265d1131b15d489c4986
SujeetYT <sujeet7982422191@gmail.com>
2022-09-10 17:54:38 +0530

    added QR Code & Copy Link & Share Socials

--- client/src/Screens/components/main/DivideOr.jsx
+++ client/src/Screens/components/main/DivideOr.jsx
@@ -2,12 +2,12 @@ import React from "react";
 const or = {
     width : "45%",
     height : "1px",
-    background : "#fff",
+    backgroundColor : "#fff",
     margin : "10px"
 
 };
 function DivideOr(){
-    return(<div style={{display : "flex", justifyContent : "center", alignItems : "center",  width : "auto", height : "20px", marginTop : "20px"}}>
+    return(<div style={{display : "flex", justifyContent : "center", alignItems : "center",  width : "100%", height : "20px", marginTop : "20px"}}>
     <div style={or}></div>Or<div style={or}></div>
     </div>
     );
